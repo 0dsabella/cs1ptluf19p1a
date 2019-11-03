4825a26453406f29d8ec829f7531a662a4f67ebd
Joseph Pancho <jnpancho120@yahoo.com>
2019-11-03 17:17:41 -0600

    Outputs # of permutations, but not sum of permutations to 9

--- project.py
+++ project.py
@@ -1,4 +1,4 @@
-import random
+import random, math, itertools
 
 def charList(n):
 
@@ -12,20 +12,51 @@ def charList(n):
         count = count - 1
         tempVowel = (random.choice(vowels))
         list.append(tempVowel)
-        
+
     #Use remaining # of counts to append a # of consonants
     for i in range(count):
         tempVowel = (random.choice(consonants))
         list.append(tempVowel)
-        
-    #So far I have got the list to become randomized and stored into a new list.
-    #I am trying to find a way to get the entire code to repeat to keep adding new combinations to newList.      
-    for i in range(n):
-      newList = []
-      random.shuffle(list)
-      newList.append(list)
 
-    return newList
-
+    return list
 #Max # of vowels is 6
-print(charList(4))
+#print(charList(4))
+
+#Formula found in https://www.calculatorsoup.com/calculators/discretemathematics/permutations.php
+def numPermutations(objects, sample):
+    return math.factorial(objects) / math.factorial(objects - sample)
+
+def sumPermutations(objects, num):
+    sample = 1
+    outputNum = 0
+    for i in (range(num)):
+        outputNum = outputNum + numPermutations(objects, sample)
+        sample = sample + 1
+    return outputNum
+
+#Referenced https://www.geeksforgeeks.org/python-program-to-convert-a-list-to-string/
+def listToString(inputList):
+    output = ""
+    return output.join(inputList)
+
+#Found itertool usage in https://kite.com/python/examples/4675/itertools-iterate-over-all-permutations-of-a-list-of-strings
+#Documentation for itertools https://docs.python.org/3/library/itertools.html
+def wordCombos(inputList):
+    print(inputList)
+
+    count = 0
+
+    trackerList = []
+    outputList = []
+
+    #Var count as tracker. Outputs permutations of characters in "trackerList",
+    #coverts to string, then appends to the "outputList"
+    while count < 9:
+        trackerList.append(inputList[count])
+        for i in itertools.permutations(trackerList):
+            outputList.append(listToString(i))
+        count = count + 1
+
+    return outputList
+
+print(wordCombos(charList(3)))
