54d0d566073c4af703ce0784c3d8261963f29a87
Joseph Pancho <jnpancho120@yahoo.com>
2019-11-03 22:49:27 -0600

    Removed factorial # functions. wordCombos outputs sum of factorials to 9

--- project.py
+++ project.py
@@ -1,7 +1,7 @@
 import random, math, itertools
 
+#Input # of vowels
 def charList(n):
-
     count = 9
     vowels = ['a', 'e', 'i', 'o', 'u', 'y']
     consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm' ,'n' ,'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z']
@@ -19,46 +19,28 @@ def charList(n):
         list.append(tempVowel)
 
     return list
-#Max # of vowels is 6
-#print(charList(4))
-
-#Formula found in https://www.calculatorsoup.com/calculators/discretemathematics/permutations.php
-def numPermutations(objects, sample):
-    return math.factorial(objects) / math.factorial(objects - sample)
-
-def sumPermutations(objects, num):
-    sample = 1
-    outputNum = 0
-    for i in (range(num)):
-        outputNum = outputNum + numPermutations(objects, sample)
-        sample = sample + 1
-    return outputNum
 
 #Referenced https://www.geeksforgeeks.org/python-program-to-convert-a-list-to-string/
 def listToString(inputList):
     output = ""
     return output.join(inputList)
 
-def permutationsTest(input):
-    output = itertools.permutations(input)
-    return output
 #Found itertool usage in https://kite.com/python/examples/4675/itertools-iterate-over-all-permutations-of-a-list-of-strings
 #Documentation for itertools https://docs.python.org/3/library/itertools.html
 def wordCombos(inputList):
+
     print(inputList)
 
-    count = 0
-
-    trackerList = []
+    count = 1
     outputList = []
 
-    #Var count as tracker. Outputs permutations of characters in "trackerList",
-    #coverts to string, then appends to the "outputList"
     while count < 9:
-        trackerList.append(inputList[count])
-        for i in itertools.permutations(trackerList):
-            outputList.append(listToString(i))
+        #"count" is the sample or "word" length. +1 added every loop
+        for i in itertools.permutations(inputList, count):
+            #appends list of permutations to the outputList
+            outputList.append(listToString(list(i)))
         count = count + 1
+    print(len(outputList))
     return outputList
 print(wordCombos(charList(3)))
 
